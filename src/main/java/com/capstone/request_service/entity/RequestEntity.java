f48160699e6e55d733a89d453940d75e54f42bec
josmyrose-03 <157198814+josmyrose-03@users.noreply.github.com>
2025-01-24 15:01:55 +0530

    Update RequestEntity.java

--- src/main/java/com/capstone/request_service/entity/RequestEntity.java
+++ src/main/java/com/capstone/request_service/entity/RequestEntity.java
@@ -37,11 +37,4 @@ public class RequestEntity {
     @Column(name = "status")
     private String status;
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-}
+   }
